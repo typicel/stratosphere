95ce5f166f7885ebfe9cfe4f2f489f764db32491
typicel <tyler.mccormick@colorado.edu>
2023-08-12 18:23:37 -0700

    posts viewing

--- src/bluesky/mod.rs
+++ src/bluesky/mod.rs
@@ -1,5 +1,5 @@
 pub mod client;
 mod command;
 mod session;
-pub use client::StratosphereApp;
+pub use client::{ClientResponse, StratosphereApp};
 pub use command::{Command, CreateRecordCommand, CreateRecordPostArgs};
